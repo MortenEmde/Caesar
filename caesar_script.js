dc6b361a354914a860038648ead89c86ae7447aa
Emde <mortenemdec@gmail.com>
2019-11-04 12:45:16 +0100

    costuct JS code and CSS and rebuild divs in html to fit functionality

--- caesar_script.js
+++ caesar_script.js
@@ -1,18 +1,17 @@
+const messageContainer = document.getElementById("message");
 
+const messageOutput = document.createElement("textarea");
+messageOutput.id = "messageOutput";
+messageOutput.disabled = true;
 
 ///text encrypt and key for encryption
-let text = document.getElementById("textBox").value;
-let key = document.getElementById("keyBox").value;
-
-let btnE = document.getElementById("encryptBtn");
-    btnE.addEventListener("click", () => {
-    console.log(key)
-    });
+const textBox = document.getElementById("textBox");
+const keyBox = document.getElementById("keyBox");
 
 const caesar = function(text, key) {
 ///make open result string
   let msgCode = "";
-  
+
 /// encrypt the capital letters into charCode in a loop 65 - 90
   for(let i = 0; i < text.length; i++){
     let textCode = text.charCodeAt(i);
@@ -20,7 +19,7 @@ const caesar = function(text, key) {
       textCode -= 65;
       textCode = (textCode + key) % 26;
       if (textCode <0){
-        textCode += 91
+        textCode += 91;
       } else {
         textCode += 65;
       }
@@ -29,7 +28,7 @@ const caesar = function(text, key) {
       textCode -= 97;
       textCode = (textCode + key) % 26;
       if (textCode <0){
-        textCode += 123
+        textCode += 123;
       } else {
         textCode += 97;
       } 
@@ -40,3 +39,53 @@ const caesar = function(text, key) {
   return msgCode;  
 }
 
+messageOutput.textContent = "Coded message appear here";
+
+let btnE = document.getElementById("encryptBtn");
+  btnE.addEventListener("click", () => {
+    text = textBox.value;
+    keyString = keyBox.value;
+    key = Number(keyString);
+    messageOutput.textContent = caesar(text,key);
+});
+
+const reverseCaesar = function(text, key) {
+///make open result string
+  let msgCode = "";
+  
+/// encrypt the capital letters into charCode in a loop 65 - 90
+  for(let i = 0; i < text.length; i++){
+    let textCode = text.charCodeAt(i);
+    if (textCode >= 65 && textCode <= 65 + 26 - 1){
+      textCode -= 65;
+      textCode = (textCode - key) % 26;
+      if (textCode <0){
+        textCode += 91;
+      } else {
+        textCode += 65;
+      }
+/// encrypt the lowercase letters into charCode in a loop 97 - 122
+    } else if (textCode >= 97 && textCode <= 97 + 26 - 1){
+      textCode -= 97;
+      textCode = (textCode - key) % 26;
+      if (textCode <0){
+        textCode += 123;
+      } else {
+        textCode += 97;
+      } 
+    }
+    /// turn charcode back to string of letters
+    msgCode += String.fromCharCode(textCode);
+  }
+  return msgCode;  
+}
+
+let btnD = document.getElementById("decryptBtn");
+  btnD.addEventListener("click", () => {
+    text = textBox.value;
+    keyString = keyBox.value;
+    key = Number(keyString);
+    messageOutput.textContent = reverseCaesar(text,key);
+});
+
+messageContainer.appendChild(messageOutput)
